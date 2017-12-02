ce03be2cc165ac9a520a029f4ecc7afed34f24db
Ben Siebert <ben@sieb3rt.nl>
2017-12-02 15:01:12 +0100

    Added second part of day1

--- 2017/day01/captcha_solver.py
+++ 2017/day01/captcha_solver.py
@@ -1,4 +1,4 @@
-def solve(nums):
+def solve(nums, look_ahead=1):
     nums = str(nums)
     if len(nums) == 0:
         return 0
@@ -8,12 +8,10 @@ def solve(nums):
 
     matching_nums = []
     for idx, curr_num in enumerate(nums):
-        try:
-            next_num = nums[idx+1]
-        except IndexError:
-            next_num = nums[0]
+        compare_with_idx = (idx + look_ahead) % len(nums)
+        compare_with_num = nums[compare_with_idx]
 
-        if curr_num == next_num:
+        if curr_num == compare_with_num:
             matching_nums.append(int(curr_num))
 
     return sum(matching_nums)
@@ -22,4 +20,8 @@ if __name__ == "__main__":
     import sys
     numbers = sys.stdin.read().strip()
     solution = solve(numbers)
+    print('SOLUTION #1')
+    print(solution)
+    solution = solve(numbers, look_ahead = len(numbers) // 2)
+    print('SOLUTION #2')
     print(solution)
--- 2017/day01/test_captcha_solver.py
+++ 2017/day01/test_captcha_solver.py
@@ -24,3 +24,15 @@ def test_works_with_ints():
 
 def test_hand():
     assert solve(3312349912343) == 3+9+3
+
+def test_halfway():
+    input_num = '1212'
+    assert solve(input_num, look_ahead = len(input_num)//2) == 6
+    input_num = '1221'
+    assert solve(input_num, look_ahead = len(input_num)//2) == 0
+    input_num = '123425'
+    assert solve(input_num, look_ahead = len(input_num)//2) == 4
+    input_num = '123123'
+    assert solve(input_num, look_ahead = len(input_num)//2) == 12
+    input_num = '12131415'
+    assert solve(input_num, look_ahead = len(input_num)//2) == 4
