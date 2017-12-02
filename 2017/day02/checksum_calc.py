849c973298b569ceeab6937a0223356c1e95a59d
Ben Siebert <ben@sieb3rt.nl>
2017-12-02 16:40:08 +0100

    Added Advent of Code 2017, day 2, part 2

--- 2017/day02/checksum_calc.py
+++ 2017/day02/checksum_calc.py
@@ -1,17 +1,20 @@
 import pytest
+from itertools import combinations, tee
 
 def convert_to_ints(file_input):
     for line in file_input:
         yield [int(num) for num in line.split()]
 
-def calc_checksum(spreadsheet):
-    checksum = 0
+def calc_checksum(spreadsheet, checksum_func):
+    return sum(checksum_func(line) for line in spreadsheet)
 
-    for line in spreadsheet:
-        difference = max(line) - min(line)
-        checksum += difference
+def diff_of_min_and_max(nums):
+    return max(nums) - min(nums)
 
-    return checksum
+def div_of_evenly_divisible(nums):
+    for combination in combinations(nums, 2):
+        if max(combination) % min(combination) == 0:
+            return max(combination) // min(combination)
 
 def test_conversion():
     spreadsheet_file = "5 1 9 5\n" \
@@ -23,14 +26,25 @@ def test_conversion():
     assert next(gen) == [7,5,3]
     assert next(gen) == [2,4,6,8]
 
-def test_calc_checksum():
+def test_diff_of_min_and_max():
+    assert diff_of_min_and_max([5,1,9,5]) == 8
+
+def test_div_of_evenly_divisible():
+    assert div_of_evenly_divisible([5,9,2,8]) == 4
+
+def test_pt1():
     spreadsheet = [[5,1,9,5], [7,5,3], [2,4,6,8]]
-    assert calc_checksum(spreadsheet) == 18
+    assert calc_checksum(spreadsheet, diff_of_min_and_max) == 18
+
+def test_pt2():
+    spreadsheet = [[5,9,2,8], [9,4,7,3], [3,8,6,2]]
+    assert calc_checksum(spreadsheet, div_of_evenly_divisible) == 9
     
 if __name__ == '__main__':
-    import sys
     with open('input', 'r') as f:
-        spreadsheet = convert_to_ints(f)
-        checksum = calc_checksum(spreadsheet)
-    print(checksum)
+        spreadsheet_pt1, spreadsheet_pt2 = tee(convert_to_ints(f))
+        checksum_pt1 = calc_checksum(spreadsheet_pt1, diff_of_min_and_max)
+        checksum_pt2 = calc_checksum(spreadsheet_pt2, div_of_evenly_divisible)
+    print(f"Result part1: {checksum_pt1}")
+    print(f"Result part2: {checksum_pt2}")
 
