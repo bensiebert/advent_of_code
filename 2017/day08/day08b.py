0acfd855dd65e852d1283b96e4320fa5877d4d11
Ben Siebert <ben@sieb3rt.nl>
2017-12-12 23:09:38 +0100

    AoC 2017 - day8 part 1 and part 2

---
+++ 2017/day08/day08b.py
@@ -0,0 +1,22 @@
+#!/usr/local/bin/python3
+from collections import defaultdict
+import sys
+
+registers = defaultdict(int)
+maxima = set()
+for line in sys.stdin:
+    words = line.split()
+    register = words[0]    
+    func = words[1]
+    amount = int(words[2])
+    cmp_register = words[4]
+    cmp_func = words[5]
+    cmp_amount = int(words[6])
+    if eval(f"registers['{cmp_register}'] {cmp_func} {cmp_amount}"):
+        if func == 'inc':
+            registers[register] += amount
+        else:
+            registers[register] -= amount
+    maxima.add(max(registers.values()))
+
+print(max(maxima))
